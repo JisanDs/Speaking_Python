9d1cb0e213895d6b0de438685a4be5a72b10bb12
Jisan Js <mdjisan01823jis@gmail.com>
2025-08-25 10:51:11 +0600

    inital commit

--- day26_speakPython15.py
+++ day26_speakPython15.py
@@ -99,13 +99,13 @@ is\_palindrome("hello") ➞ False
 Check first and last character, then recurse on the substring.
 """
 
-# def is_palindrome(s):
-#     if len(s) == 0 or len(s) == 1:
-#         return True
-#     elif s[0] != s[-1]:
-#         return False
-#     return is_palindrome(s[1:-1])
+def is_palindrome(s):
+    if len(s) == 0 or len(s) == 1:
+        return True
+    elif s[0] != s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
 
-# print(is_palindrome("madam"))
-# print(is_palindrome("hello"))
-# print(is_palindrome("racecar"))
+print(is_palindrome("madam"))
+print(is_palindrome("hello"))
+print(is_palindrome("racecar"))
