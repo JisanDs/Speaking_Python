8a0b0531b62ba6013fe77f0a19169846097e220c
JisanDs <mdjisan01823jis@gmail.com>
2025-09-18 00:57:52 +0600

    It's a library

--- FileUtilites.py
+++ FileUtilites.py
@@ -1,6 +1,9 @@
+"""This library contains basic but useful file operations function."""
+
 import json
 
 def load_json(filename):
+    """This function load json file and json file not exist or file empty it's return a new dict."""
     try:
         with open(filename) as file:
             return json.load(file)
