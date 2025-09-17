a128bb10225a3270324637f103150f6ca1717f9d
JisanDs <mdjisan01823jis@gmail.com>
2025-09-18 01:00:21 +0600

    rm

--- FileUtillites.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_json(filename):
-    try:
-        with open(filename) as file:
-            return json.load(file)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        return {}
-        
