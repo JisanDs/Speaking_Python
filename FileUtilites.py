0cd096e54b9eabbe1bf535696a384f2fa740bc8a
Jisan Js <mdjisan01823jis@gmail.com>
2025-09-21 22:26:26 +0600

    add new save_json func & add encoding in load_json

--- FileUtilites.py
+++ FileUtilites.py
@@ -5,8 +5,14 @@ import json
 def load_json(filename):
     """This function load json file and json file not exist or file empty it's return a new dict."""
     try:
-        with open(filename) as file:
+        with open(filename, encoding="utf-8") as file:
             return json.load(file)
     except (FileNotFoundError, json.decoder.JSONDecodeError):
         return {}
-        
+    
+def save_json(obj, filename):
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(obj, filename)
+    except UnicodeDecodeError:
+        pass
