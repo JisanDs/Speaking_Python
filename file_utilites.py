b0e5bf3b86e571175cf88676e0d8e962facac61e
Jisan Js <mdjisan01823jis@gmail.com>
2025-09-29 13:34:21 +0600

    rename file

---
+++ file_utilites.py
@@ -0,0 +1,18 @@
+"""This library contains basic but useful file operations function."""
+
+import json
+
+def load_json(filename):
+    """This function load json file and json file not exist or file empty it's return a new dict."""
+    try:
+        with open(filename, encoding="utf-8") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return {}
+    
+def save_json(obj, filename):
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(obj, file, indent=2)
+    except UnicodeDecodeError:
+        pass
